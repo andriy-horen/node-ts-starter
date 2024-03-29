7d184e31fc575bbd36e9c599c139a2ce0c5cc148
Andriy Horen <go.andriy@gmail.com>
2024-03-29 02:35:44 +0100

    Add eslint

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,17 +2,17 @@ import { main } from '.';
 
 describe('main method', () => {
   it('should call console.log with the correct message', () => {
-    // Spy on console.log
     const consoleSpy = jest.spyOn(console, 'log');
 
-    // Prevent actually logging to keep test output clean
-    consoleSpy.mockImplementation(() => {});
+    consoleSpy.mockImplementation(() => {
+      // prevent actually logging to keep test output clean
+    });
 
     main();
 
     expect(consoleSpy).toHaveBeenCalledWith('it works!');
 
-    // Clean up and remove the spy
+    // clean up and remove the spy
     consoleSpy.mockRestore();
   });
 });
